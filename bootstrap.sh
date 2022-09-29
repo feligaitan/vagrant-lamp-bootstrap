c94583f3f252d8ff42344f921f90e15697a9d407
Felipe.Gaitan <felipe.gaitan@quoruminfotech.com>
2022-09-29 14:43:58 -0400

    simplest php lamp

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # Use single quotes instead of double quotes to make it work with special-character passwords
-PASSWORD='12345678'
-PROJECTFOLDER='myproject'
+PROJECTFOLDER=''
 
 # create project folder
 sudo mkdir "/var/www/html/${PROJECTFOLDER}"
@@ -14,21 +13,6 @@ sudo apt-get -y upgrade
 # install apache 2.5 and php 5.5
 sudo apt-get install -y apache2
 sudo apt-get install -y php5
-
-# install mysql and give password to installer
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
-sudo apt-get -y install mysql-server
-sudo apt-get install php5-mysql
-
-# install phpmyadmin and give password(s) to installer
-# for simplicity I'm using the same password for mysql and phpmyadmin
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-sudo apt-get -y install phpmyadmin
 
 # setup hosts file
 VHOST=$(cat <<EOF
@@ -48,13 +32,6 @@ sudo a2enmod rewrite
 
 # restart apache
 service apache2 restart
-
-# install git
-sudo apt-get -y install git
-
-# install Composer
-curl -s https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
 
 # cleanup
 sudo apt-get -y autoremove
